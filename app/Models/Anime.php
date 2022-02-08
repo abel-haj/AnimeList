119f44e47fe1aedf4030b50ecf31eaf3d7ca4f46
Abdelaziz <techproduct@hotmail.com>
2022-02-08 03:06:24 +0100

    fixing namespaces and filling details

--- app/Models/Anime.php
+++ app/Models/Anime.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
@@ -8,4 +8,18 @@ class Anime extends Model
 {
     // renaming primary key name
     protected $primaryKey = 'anime_id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'anime_name',
+        'anime_image',
+        'anime_featured_image',
+        'anime_release_date',
+        'anime_rating',
+        'anime_votes',
+    ];
 }
--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -1,10 +1,17 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'comment_name', 'comment_email', 'comment_details',
+    ];
 }
